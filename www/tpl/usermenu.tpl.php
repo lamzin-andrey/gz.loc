4d04a268bc8c94aa2c22367be5356c0cc6238374
Andrey Lamzin <lamzin.an@gmail.com>
2017-08-12 13:15:15 +0300

    Кнопки пользователя для мобилы

--- www/tpl/usermenu.tpl.php
+++ www/tpl/usermenu.tpl.php
@@ -1,34 +1,35 @@
 <div class="buttons">
-		<table><tr>
-			<?php if ($showAddAdvBtn) {?><td>
+		
+			<?php if ($showAddAdvBtn) {?><div class="left usermenu-mt">
 				<a href="/podat_obyavlenie" class="hidelink">
 					<div class="add button">
 						Подать объявление
 					</div>
 				</a>
-			</td><?php }?>
-			<?php if ($showCabBtn) {?><td>
+			</div><?php }?>
+			<?php if ($showCabBtn) {?><div class="left usermenu-mt">
 				<a href="/cabinet" id="authlink" class="hidelink">
 					<div class="auth button">
 						<img src="/images/encrypted.png" class="w16"/> <span style="vertical-align:top; display:block-inline" class="">Мои объявления</span>
 					</div>
 				</a>
-			</td><?php }?>
-			<?php if ($showExitBtn) {?><td>
+			</div><?php }?>
+			<?php if ($showExitBtn) {?><div class="left usermenu-mt">
 				<a href="/logout" id="logoutlink" class="hidelink" title="Выход">
 					<div class="out button">
 						<img src="/images/out.png" class="w20"/>
 					</div>
 				</a>
-			</td><?php } ?>
-			<?php if ($showSetBtn) {?><td>
+			</div><?php } ?>
+			<?php if ($showSetBtn) {?><div class="left usermenu-mt">
 				<a href="/cabinet/setting" class="hidelink" title="Ваши данные">
 					<div class="out button">
 						<img src="/images/set.png" class="w20"/>
 					</div>
 				</a>
-			</td><?php } ?>
-		</tr></table>
+			</div><?php } ?>
+			<div class="both"></div>
+		
 	</div>
 	<?php FV::$obj = @$aform; ?>
 	<noindex>
