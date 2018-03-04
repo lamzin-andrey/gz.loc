ed533769b8d4b1c0b3d7c3e052cb96ebe2f0570d
Andrey Lamzin <lamzin.an@gmail.com>
2018-03-04 10:06:13 +0300

    Газели и купи через единый прокси

--- tools/внешний-скрипт-яндекс/rkresult.php
+++ tools/внешний-скрипт-яндекс/rkresult.php
@@ -1,6 +1,8 @@
 <?php
 file_put_contents(__DIR__ . '/rklog.log', print_r($_POST, 1), FILE_APPEND);
 require_once __DIR__ . '/Request.php';
+require_once __DIR__ . '/pgetdomain.php';
 $r = new Request();
-$r = $r->execute('http://test.gazel.me/rkresult', $_POST);
+$r = $r->execute($domain . '/rkresult', $_POST);
+//$r = $r->execute('http://gazel.me/rkresult', $_POST);
 echo $r->responseText;
