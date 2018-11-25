81d77ff1457fbc7f24d4758181228e09a5d4f601
Andrey Lamzin <lamzin.an@gmail.com>
2018-11-25 16:26:59 +0300

    www/unexecute_sql/add_date_update.sql

--- www/unexecute_sql/add_date_update.sql
+++ www/unexecute_sql/add_date_update.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `main` ADD COLUMN date_update DATETIME;
-UPDATE main SET date_update = '2018-09-11 09:35:00';
+ALTER TABLE `main` ADD COLUMN orly VARCHAR(512) COMMENT 'Оригинальный url объявления, заполняется вручную';
