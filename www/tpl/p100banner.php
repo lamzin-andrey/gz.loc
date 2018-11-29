9f4e5156c490201fc47b58430aa807e357259978
Andrey Lamzin <lamzin.an@gmail.com>
2018-11-29 09:46:34 +0300

    Баннер для qp2t 320 и десктоп

--- www/tpl/p100banner.php
+++ www/tpl/p100banner.php
@@ -1,11 +1,11 @@
 <?php if ( strpos($_SERVER['HTTP_USER_AGENT'], 'yandex.com') === false ): ?>
 <noindex>
 <div class="p100banner">
-	<section>
+	<a href="http://qp2t.ru/promo" rel="nofollow"><section>
 		<img src="/images/s.png">
 		<b>ОБЪЯВЛЕНИЕ ПОДНИМЕТСЯ САМО!</b>
 		<u></u>
-	</section>
+	</section></a>
 	
 </div>
 </noindex>
