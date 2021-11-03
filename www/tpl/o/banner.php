54b90f3487582d1028c82af6a4d42a8a83fdfdd2
andrey <lamzin.an@gmail.com>
2021-11-03 21:01:31 +0300

    Change banner design

--- www/tpl/o/banner.php
+++ www/tpl/o/banner.php
@@ -1,8 +1,8 @@
 <?php if ( strpos($_SERVER['HTTP_USER_AGENT'], 'yandex.com') === false ): ?>
 <noindex>
-<div class="osagobanner s">
+<div class="osagobanner_sber s">
 	<a href="/osago"><section>
-		<b>Купить ОСАГО онлайн</b>
+		<img src="/images/o/s.png"><b> Сбер ОСАГО от 3 200 Р</b>
 	</section></a>
 </div>
 </noindex>
