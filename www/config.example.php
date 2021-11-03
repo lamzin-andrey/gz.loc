a8d4ce3a2c5147e9a8018250716d8730934ba965
andrey <lamzin.an@gmail.com>
2021-11-03 21:19:36 +0300

    Change banner design

--- www/config.example.php
+++ www/config.example.php
@@ -1,27 +1,56 @@
 <?php
 #================ db====================================================
-mysql_pconnect("localhost", "user", "*****") or die('connect error');
-mysql_select_db("gz") or die('error select db gz');
+mysql_pconnect("localhost", "123860", "***") or die('connect error');
+mysql_select_db("7****") or die('error select db gz');
 mysql_query("SET NAMES UTF8");
 #================ constant==============================================
 define("DR", $_SERVER["DOCUMENT_ROOT"]);
 define("TPLS", DR . '/tpl');
 define("MAX_WIDTH", 240);
 define("MAX_HEIGHT", 240);
-define("ADMIN_PHONE", '000000');
-define('SITE_EMAIL', '***@**.ru');
-define('NOTICE_EMAIL', '***@***.ru');
-define('SITE_NAME', 'gazel.me');
+define("ADMIN_PHONE", '+***');
+define("SITE_EMAIL", 'admin@gazel.me');
+define("SITE_NAME", 'gazel.me');
+define('NOTICE_EMAIL', 'n@gmail.com');
 define("H1_BEG", "Заказ ГАЗели в ");
 define('AUTO_MODERATION_ON', true);
 define('MONTH_IN_SEC', 30*24*3600);
 
 define('SMS_INTERVAL', 63);//product 900
 define('SMS_SERVICE_ON', true);
-define('SMS_SERVICE_LOGIN', '****');
-define('SMS_SERVICE_PASSWORD', '***');
+//epochta
+define('SMS_SERVICE_LOGIN', '@mail.ru');
+define('SMS_SERVICE_PASSWORD', '****');
 
-define('SMS_PILOT_API_KEY', '*****');
+//
+define('SMS_PILOT_API_KEY', '***');
+
+define('EGATE_PWD', '****');
+
+define('ASSETS_VERSION', 'b');
+
+//Связано с оплатой
+define('CODE_DEC_FOR_UP', 3);  //код операции списания за поднятие
+define('CODE_INC_FOR_UP', 2);  //код операции покупки возможности поднятий
+define('CODE_GIFT_MONTH', 1);  //код операции ежемесячный подарок от сайта
+//Связано с оплатой через яндекс
+define('YAM', '****');
+define('YAKEY', '****');
+define('PROXY_YAM_CHECK_URL', 'https://****.ru/portfolio/web-razrabotka/saity/fastxampp/js/phpcheck.php');//На этот url отправляем запрос с одним параметром, этот сервер отправляыет нам его же отдельным запросом. После того как отправка нашего запроса завершена, лезем в лог, который оставляет скрипт-приёмник ответного запроса с сервера и сравнивает отправленное и полученное значение
+#http://test.gazel.me/pcf
+
+//Связано с оплатой через робокассу
+//Связано с оплатой через робокассу
+define('RK_P1', '***');//test ***
+define('RK_P1T', '****');//test 
+define('RK_P2', '****');//test ***
+define('RK_P2T', '****');//test 
+define('RK_ID', 'gazelme');
+//define('RK_TEST_MODE', 1);
+//если хочешь отключить оплату в принципе - закомментируй! (будет каждый месяц раздавать всем по 100 бесплатных поднятий)
+//define('PAY_ENABLED', true);
+//если хочешь отключить оплату для физ.лиц - закомментируй!
+define('PAY_PHIS_ENABLED', true);
 
 //если хочешь отключить captcha на форме поднятия объявления - закомментируй
 define('CAPTCHA_UPFORM_OFF', true);
@@ -32,37 +61,8 @@ define('CAPTCHA_ADV_AUTH_OFF', true);
 //если хочешь отключить captcha на форме подачи объявления неавторизованным и авторизованым - закоментируй
 define('CAPTCHA_ADV_AL_OFF', true);
 
+define('ADV_GATE_PWD', '*****');
 
-define('ASSETS_VERSION', '12');
+define('POLITICS_DOC', '/images/Politika_zashity_i_obrabotki_personalnyh_dannyh_2019-08-14.doc');
 
-//Связано с оплатой
-define('CODE_DEC_FOR_UP', 3);  //код операции списания за поднятие
-define('CODE_INC_FOR_UP', 2);  //код операции покупки возможности поднятий
-define('CODE_GIFT_MONTH', 1);  //код операции ежемесячный подарок от сайта
-//Связано с оплатой через яндекс
-define('YAM', '****');
-define('YAKEY', '****');
-define('PROXY_YAM_CHECK_URL', '****/phpcheck.php');//На этот url отправляем запрос с одним параметром, этот сервер отправляыет нам его же отдельным запросом. После того как отправка нашего запроса завершена, лезем в лог, который оставляет скрипт-приёмник ответного запроса с сервера и сравнивает отправленное и полученное значение
-#http://test.gazel.me/pcf
-
-//Связано с оплатой через робокассу
-define('RK_P1', '****');
-define('RK_P1T', '');
-define('RK_P2', '');
-define('RK_P2T', '');
-define('RK_ID', 'gazelme');
-define('RK_TEST_MODE', 1);
-
-
-//если хочешь отключить оплату в принципе - закомментируй! (будет каждый месяц раздавать всем по 100 бесплатных поднятий)
-define('PAY_ENABLED', true);
-//если хочешь отключить оплату для физ.лиц - закомментируй!
-//define('PAY_PHIS_ENABLED', true);
-
-//если не надо проверять прокси для яндекса, должна быть эта константа
-define('CHECK_PROXY_OFF', 1);
-
-
-//Пароль для запросов из us с сайта объявлений
-define('ADV_GATE_PWD', '****');
 //mysql_close();
