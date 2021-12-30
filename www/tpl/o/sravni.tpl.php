232f9d3e258858840ee079337ca26f03e2f651f4
andrey <lamzin.an@gmail.com>
2021-12-30 18:23:44 +0300

    Banner update

--- www/tpl/o/sravni.tpl.php
+++ www/tpl/o/sravni.tpl.php
@@ -3,7 +3,7 @@ href="https://go.sravni.ru/aff_c?aff_id=2479&offer_id=1064&source=3541&out=https
 	target="_blank">
 	<section>
 		<div class="sravni-banner">Оформить ОСАГО в Сбере онлайн от <b class="sravni-tcolor">3 200 Р</b></div>
-		<div class="sravni-sub">Для легковых авто и ГАЗ-елей.</div>
+		<div class="sravni-sub">Для легковых авто и ГАЗелей.</div>
 		<div class="sravni-sub">Предложение от Сбера не гарантированно.</div>
 		<div class="sravni-green">Рассчитать в СравниРу</div>
 	</section>
