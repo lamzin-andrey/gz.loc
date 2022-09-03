1bc9795aefae49a2dfa81b6b26e68e5a8bd55d86
andrey <lamzin.an@gmail.com>
2022-09-03 10:37:11 +0300

    es5-shim пожалуй, давно пора)

--- www/tpl/master.tpl.php
+++ www/tpl/master.tpl.php
@@ -16,8 +16,8 @@
 			var token = '<?=@$_SESSION['utoken']; ?>';
 			var uid   = '<?=@$_SESSION['uid']?>';
 		</script>
-		<script type="text/javascript" src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js" charset="utf-8"></script>
-		<script type="text/javascript" src="//yastatic.net/share2/share.js" charset="utf-8"></script>
+		<?php /*script type="text/javascript" src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js" charset="utf-8"></script>
+		<script type="text/javascript" src="//yastatic.net/share2/share.js" charset="utf-8"></script */?>
 							
 		<? if ($_SERVER['HTTP_HOST'] == 'gz.loc'):?>
 			<script type="text/javascript" src="/js/test.js?a=0"></script>
