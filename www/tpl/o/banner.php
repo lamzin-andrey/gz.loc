ac5bc174d67922adebd23fca88152d061264bbf9
andrey <lamzin.an@gmail.com>
2022-09-04 09:06:28 +0300

    Дни денёчки

--- www/tpl/o/banner.php
+++ www/tpl/o/banner.php
@@ -2,7 +2,7 @@
 <noindex>
 <div class="osagobanner_sber s">
 	<a href="/osago"><section>
-		<img src="/images/o/f.png"><b>ОСТАЛОСЬ 10 дней до подорожания ОСАГО! Успей оформить!</b>
+		<img src="/images/o/f.png"><b>ОСТАЛОСЬ <?=13 - intval(date('d'))?> дней до подорожания ОСАГО! Успей оформить!</b>
 	</section></a>
 </div>
 </noindex>
