761f63c6666d3f3d5104016e72feaa348cf82571
andrey <lamzin.an@gmail.com>
2022-11-06 18:06:53 +0300

    Работы по добавлению erid в ссылки сайта. Добавлено сохранение invoice  на заглушке с test.loc

---
+++ www/lib/classes/erir/test.loc/www/ordyaapi/v1/invoice/index.php
@@ -0,0 +1,9 @@
+<?php
+// invoice success
+header('Content-Type: application/json');
+echo '{
+  "request_id": "539b1621-386b-4d5f-856e-21678d16db88",
+  "object_id": "1",
+  "object_type": "invoice",
+  "status": "Request accepted by ORD"
+}';
